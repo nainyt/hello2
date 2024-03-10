9d37cc7fed83e325b2167d1deea91a8f95c163f1
unknown <nainkhan12374@gmail.com>
2024-03-10 11:06:30 +0500

    new

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('products api running new deploy');
+    res.send('Hasnain');
 });
 
 
 
-app.listen(8080, () => {
+app.listen(8000, () => {
     console.log('Server is listenin on PORT :' + PORT);
 })
